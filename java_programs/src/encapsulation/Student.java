58afd6a931291396e2c53baf8fb9e88c3fe60b22
lenovo <lenovo@DESKTOP-8L1R360.lan>
2025-08-05 21:41:02 +0530

    Encapsulation in the java program

--- java_programs/src/encapsulation/Student.java
+++ java_programs/src/encapsulation/Student.java
@@ -2,8 +2,34 @@ package encapsulation;
 
 public class Student {
 
-	int sid;
-	String sname;
-	float avg;
+	private int sid;
+	private String sname;
+	private float avg;
+	
+	
+	
+	public int getSid() {
+		return sid;
+	}
+	public void setSid(int sid) {
+		this.sid = sid;
+	}
+	public String getSname() {
+		return sname;
+	}
+	public void setSname(String sname) {
+		this.sname = sname;
+	}
+	public float getAvg() {
+		return avg;
+	}
+	public void setAvg(float avg) {
+		this.avg = avg;
+	}
+	
 
+	@Override
+	public String toString() {
+		return "Student [sid=" + sid + ", sname=" + sname + ", avg=" + avg + "]";
+	}
 }
--- java_programs/src/encapsulation/StudentDemo.java
+++ java_programs/src/encapsulation/StudentDemo.java
@@ -4,7 +4,23 @@ public class StudentDemo {
 
 	public static void main(String[] args) {
 		Student s=new Student();
-		System.out.println("");
+		s.setSid(101);
+		s.setSname("Rajan");
+		s.setAvg(97.5f);
+		
+		System.out.println("Student id:"+s.getSid());
+		System.out.println("Student name:"+s.getSname());
+		System.out.println("Student Average:"+s.getAvg());
+		
+		System.out.println(s); // invoke the object
+		
+		Student s1=new Student();
+		s1.setSid(7);
+		s1.setSname("Dhoni");
+		s1.setAvg(100.0f);
+		System.out.println(s1);
+		
+		
 
 	}
 
