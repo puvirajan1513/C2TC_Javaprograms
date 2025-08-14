c088d22df9c1a517ae2ae981e087f21744cb3e5f
lenovo <lenovo@DESKTOP-8L1R360>
2025-08-14 14:24:40 +0530

    parameter condition

---
+++ java_programs/src/day3/Product.java
@@ -0,0 +1,42 @@
+package day3;
+
+public class Product {
+	private int pid;
+	private String product_name;
+	private float price;
+	public int getPid() {
+		return pid;
+	}
+	public Product()
+	{
+		this.pid=101;
+		this.product_name="keys";
+		this.price=245.6f;
+	}
+	public Product(int pid,String product_name,float price)
+	{//parametter constructor
+		this.pid=pid;
+		this.product_name=product_name;
+		this.price=price;
+	}
+	public void setPid(int pid) {
+		this.pid = pid;
+	}
+	public String getProduct_name() {
+		return product_name;
+	}
+	public void setProduct_name(String product_name) {
+		this.product_name = product_name;
+	}
+	public float getPrice() {
+		return price;
+	}
+	public void setPrice(float price) {
+		this.price = price;
+	}
+	@Override
+	public String toString() {
+		return "Product [pid=" + pid + ", product_name=" + product_name + ", price=" + price + "]";
+	}
+
+}
---
+++ java_programs/src/day3/ProductDemo.java
@@ -0,0 +1,25 @@
+package day3;
+import java.util.*;
+
+public class ProductDemo {
+
+	public static void main(String[] args) {
+		Product p1=new Product();
+		Product p2=new Product(102,"mouse",456.6f);
+		
+		
+		Scanner s=new Scanner(System.in);
+		int sid=s.nextInt();
+		s.nextLine();
+		String product_name=s.nextLine();
+		float price=s.nextFloat();
+		Product p3=new Product(sid,product_name,price);
+		System.out.println(p1);
+		System.out.println(p2);
+		System.out.println(p3);
+		
+		
+		
+	}
+
+}
