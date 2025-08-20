f418aa4a458aeefabfb9e78a6c57a65cf5845de4
lenovo <lenovo@DESKTOP-8L1R360>
2025-08-20 13:40:01 +0530

    final keyword

---
+++ java_programs/src/day6/usingfinal/FinalClassDemo.java
@@ -0,0 +1,24 @@
+package day6.usingfinal;
+
+public class FinalClassDemo {
+	final class FinalClass {
+		void show() {
+			System.out.println("Final class cannot be inherited");
+		}
+	}
+
+	//can't create child classes from Final class, Ex: String, Wrapper Classes, System, Scanner, Number are Final classes
+	class FinalChildClass extends FinalClass {
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// Create the object of final class
+		FinalClass f1 = new FinalClass(); // Call show() method using object reference variable ab.
+		f1.show();
+
+
+	}
+
+
+}
---
+++ java_programs/src/day6/usingfinal/FinalMethodClassTest.java
@@ -0,0 +1,14 @@
+package day6.usingfinal;
+
+public class FinalMethodClassTest {
+	
+	// Compile time error because we cannot override the final method 
+				/*
+				 * @Override void show() {
+				 * 
+				 * System.out.println("This is the final method of FinalMethodEx class"); }
+				 */
+
+
+
+}
