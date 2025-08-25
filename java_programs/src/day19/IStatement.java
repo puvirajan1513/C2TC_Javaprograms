41045c7a10740fdc9070632e7e8447b166cfb27f
lenovo <lenovo@DESKTOP-8L1R360>
2025-08-25 14:57:08 +0530

    Day 19 of my java program

---
+++ java_programs/src/day19/IStatement.java
@@ -0,0 +1,6 @@
+package day19;
+
+@FunctionalInterface // interface is declared with only one abstract method
+interface IStatement {
+	public String show();// abstract Method
+}
---
+++ java_programs/src/day19/IsOdd.java
@@ -0,0 +1,6 @@
+package day19;
+
+@FunctionalInterface
+interface IsOdd{
+	public boolean checkOdd(int a);
+}
